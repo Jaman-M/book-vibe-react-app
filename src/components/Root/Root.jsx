7bbb9bb1f5acb34e608c6b1f1759f853dd8fcf1b
Jaman-M <jaman.manik1@gmail.comm>
2025-02-02 06:37:09 +0800

    create and setup the project: react router dom, tailwind css, daisyUI, router

---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,15 @@
+
+const Root = () => {
+    return (
+        <div>
+            <h2>showing from root</h2>
+            <button className="btn">Button</button>
+            
+            <button className="btn btn-accent">Accent</button>
+            <button className="btn btn-ghost">Ghost</button>
+            <button className="btn btn-link">Link</button>
+        </div>
+    );
+};
+
+export default Root;
