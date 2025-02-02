ccc4d3f18312d2aad60e641194549a5aec4630c2
Jaman-M <jaman.manik1@gmail.comm>
2025-02-03 07:27:04 +0800

    save mark as read data to somewhere, addToDbjs, practicing for localStorage

--- src/components/BookDetail/BookDetail.jsx
+++ src/components/BookDetail/BookDetail.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
+import { addToStoredReadList } from '../../utility/addToDb';
 
 const BookDetail = () => {
 
-    const {bookId} = useParams();
+    const { bookId } = useParams();
 
     const data = useLoaderData();
     const id = parseInt(bookId)
@@ -14,15 +15,20 @@ const BookDetail = () => {
     const book = data.find(book => book.bookId === id);
     // console.log(book);
 
-    const {bookId: currentBookID, image} = book;
+    const { bookId: currentBookID, image } = book;
+
+    const handleMarkAsRead = (id) => {
+        addToStoredReadList(id)
+
+    }
 
     return (
         <div className='my-12'>
             <h2>Book Details: {bookId}</h2>
             <img className='w-36' src={image} alt="" />
             <br />
-            <button className='btn btn-outline mr-4'>Read</button>
-            <button className='btn btn-accent'>Wish List</button>
+            <button onClick={() => handleMarkAsRead(bookId)} className='btn btn-outline mr-4'> Mark as Read</button>
+            <button className='btn btn-accent'>Add to Wish-List</button>
         </div>
     );
 };
